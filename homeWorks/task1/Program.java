028b76ff8c4e2bca05713ee47d86f8669163e0ee
GubaydullinAidar <gubaydu11in.aidar@gmail.com>
2017-03-05 21:29:00 +0300

    task1

--- homeWorks/task1/Program.java
+++ homeWorks/task1/Program.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 class Program {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		int s = 1;
+		int a = scanner.nextInt();
+
+		while (a != -1) {
+
+			int num = 1;
+
+			if (a == 2 || a == 3) {
+				num = a;
+			}
+
+			for (int i = 2; i*i <= a; i++) {
+
+				if (a % i == 0) {
+					num = 1;
+					break;
+				} else {
+					num = a;
+				}
+
+			}
+
+			s *= num;
+			a = scanner.nextInt();
+		}
+
+		if (s == 1) {
+			s = 0;
+		}
+
+		System.out.println(s);
 	}
 }
