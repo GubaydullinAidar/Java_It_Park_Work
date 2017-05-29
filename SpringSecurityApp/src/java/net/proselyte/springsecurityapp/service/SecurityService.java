b093a703c6e92b54f6f9d6ece63f452fa34e375d
gubaydullinaidar <gubaydu11in.aidar@gmail.com>
2017-05-29 03:21:22 +0300

    springsecurityapp

---
+++ SpringSecurityApp/src/java/net/proselyte/springsecurityapp/service/SecurityService.java
@@ -0,0 +1,11 @@
+package net.proselyte.springsecurityapp.service;
+
+/**
+ * Service for Security.
+ */
+public interface SecurityService {
+
+    String findLoggedInUsername();
+
+    void autoLogin(String username, String password);
+}
---
+++ SpringSecurityApp/target/classes/main/resources/database.sql
@@ -0,0 +1,24 @@
+-- Table: users
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+--Table roles
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+--Table for mapping user and roles: user_roles
+CREATE TABLE user_roles (
+  user_id INTEGER NOT NULL UNIQUE REFERENCES users(id),
+  roles_id INTEGER NOT NULL UNIQUE REFERENCES roles(id)
+);
+
+--Insert data
+INSERT INTO users VALUES (1, 'proselyte', '$2a$04$nUcZLoRDCFzl6cwsgNVVpeiPGbellIEGldOTUu6jAS8zNev4L2h9.');
+
+INSERT INTO roles VALUES (1, 'ROLE_USER');
+INSERT INTO roles VALUES (2, 'ROLE_ADMIN')
